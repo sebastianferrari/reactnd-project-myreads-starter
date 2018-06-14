45b30b36b2a5a7dacbaf26ba295342b7f712fee4
Sebastian Ferrari <sebastianferrari@live.com>
2018-06-13 23:57:24 -0500

    feat: Start to use react-router-dom
    Just only implemented the basics for the two URLs requested by now.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
+import { Route } from "react-router-dom";
 
 class BooksApp extends React.Component {
   state = {
@@ -16,7 +17,7 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
+        <Route path='/search' render={() => (
           <div className="search-books">
             <div className="search-books-bar">
               <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
@@ -37,7 +38,8 @@ class BooksApp extends React.Component {
               <ol className="books-grid"></ol>
             </div>
           </div>
-        ) : (
+        )} />
+        <Route exact path='/' render={() => (
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
@@ -196,8 +198,8 @@ class BooksApp extends React.Component {
             <div className="open-search">
               <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
             </div>
-          </div>
-        )}
+          </div>          
+        )} />
       </div>
     )
   }
